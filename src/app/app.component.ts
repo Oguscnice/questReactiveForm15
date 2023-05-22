5dbe9d335ceeda111311b872926083c6732d70b0
Oguscnice <guillaume_cometto@hotmail.fr>
2023-05-22 15:21:59 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'questReactivForm15';
+  constructor(public fb: FormBuilder) {}
+
+  submit: boolean = false;
+
+  userForm = this.fb.group({
+    username: [''],
+    email: [''],
+    password: [''],
+    street: [''],
+    zip: [''],
+    city: [''],
+  });
+
+  onSubmit() {
+    this.submit = true;
+    console.log(this.userForm.value);
+    
+  }
 }
